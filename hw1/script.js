481b5e036624d554ccab644d90f81d240626950c
YuraJian <ur.duimja@gmail.com>
2025-02-10 14:54:30 +0200

    create files for task

--- hw1/script.js
+++ hw1/script.js
@@ -1,6 +1,6 @@
 let userAge = 14;
-let userName = 'Yura';
-let userSurname = 'Demianchuk';
+let userName = "Yura";
+let userSurname = "Demianchuk";
 
 let newUser;
 let productsList;
@@ -8,4 +8,14 @@ let isUserOnline;
 let salary;
 let mouseCoords;
 
-console.log('Hello World!')
+console.log("Hello World!");
+
+let count = 10;
+console.log(count);
+count = 25;
+console.log(count);
+
+const myName = "Yura";
+console.log(myName);
+// myName = "Roma";
+console.log(myName);
