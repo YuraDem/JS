253899d6b76b561c9bd1d017c77ae7d5843c8199
YuraJian <ur.duimja@gmail.com>
2025-02-10 12:38:07 +0200

    complete task

---
+++ hw1/script.js
@@ -0,0 +1,11 @@
+let userAge = 14;
+let userName = 'Yura';
+let userSurname = 'Demianchuk';
+
+let newUser;
+let productsList;
+let isUserOnline;
+let salary;
+let mouseCoords;
+
+console.log('Hello World!')
