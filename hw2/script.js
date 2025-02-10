9651dbc69025ad8ed5e67b206a32353cb2470cec
YuraJian <ur.duimja@gmail.com>
2025-02-10 15:32:03 +0200

    complete task

---
+++ hw2/script.js
@@ -0,0 +1,56 @@
+const a = 5;
+const b = 10;
+
+console.log("addition", a + b);
+console.log("subtraction", a - b);
+console.log("division", a / b);
+console.log("multiplication", a * b);
+console.log("exponentiation", a ** b);
+
+// const firstNumber = Number(prompt("Enter first number!"));
+// const secondNumber = Number(prompt("Enter second number!"));
+
+// if (firstNumber > secondNumber) {
+//   alert(`Number ${firstNumber} is bigger then ${secondNumber}`);
+// } else if (firstNumber < secondNumber) {
+//   alert(`Number ${secondNumber} is bigger then ${firstNumber}`);
+// } else {
+//   alert(`Number ${firstNumber} equal ${secondNumber}`);
+// }
+
+// const firstUserValue = Number(prompt("Enter your first value"));
+// const secondUserValue = Number(prompt("Enter your second value"));
+// const chooseOperator = prompt("Choose one of these operators ( /, *, -, + )");
+
+// switch (chooseOperator) {
+//   case "/":
+//     alert(firstUserValue / secondUserValue);
+//     break;
+//     case '*':
+//         alert(firstUserValue * secondUserValue);
+//     break;
+//     case '-':
+//         alert(firstUserValue - secondUserValue);
+//     break;
+//     case '+':
+//         alert(firstUserValue + secondUserValue);
+//     break;
+//     default:
+//         alert(`I don't know this operator :()`)
+// }
+
+// const visitorAge = Number(prompt("How old are you?"));
+
+
+// if (visitorAge >= 60 && visitorAge <= 80 || visitorAge >= 12 && visitorAge <= 17) {
+//     const isWithParents = confirm('Are you with parents?');
+//     if(isWithParents) {
+//         alert('Welcome!')
+//     } else {
+//         alert(`Sorry today we closed for you!`)
+//     }
+// } else if (visitorAge >= 18 && visitorAge < 60) {
+//     alert('Welcome!')
+// } else {
+//     alert('Oh no you cant visit our rollercoaster :(')
+// }
